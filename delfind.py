62dde50d92d9451510d7cd1391180ae93faf493d
tdfyoder <tdfyoder@gmail.com>
2019-08-21 16:56:49 -0400

    Added PAM site logic for on target vs off target annotation

--- delfind.py
+++ delfind.py
@@ -20,6 +20,7 @@ import pysam
 
 bam = sys.argv[1]
 path = sys.argv[2]
+Target = sys.argv[3]
 
 CIGAR_list = []
 String_list = [1,2]
@@ -28,8 +29,8 @@ varlen_list = []
 var_list = []
 cig_pack = []
 ch_list = []
-
-
+CIG_dict = {}
+ID = []
 cig_df = pd.DataFrame()
 
 
@@ -39,6 +40,7 @@ for read in bamFP:
     if( not( read.is_unmapped ) ):
         cig_string = read.cigar
         CIGAR_list.append(cig_string)
+        cigarette = read.cigarstring
 
         for index, tup in enumerate(cig_string):
             ch = cig_string[index][0]
@@ -49,7 +51,7 @@ for read in bamFP:
 
                 # pos = str(sum([t[1] for t in slice_l]))
                 pos2 = str(sum([t[1] for t in slice_m]))
-                # indelicous = [t[0] for t in slice_m]
+
                 var_len = slice_l[-1][1]
                 variant = slice_l[-1][0]
 
@@ -65,29 +67,49 @@ for read in bamFP:
 
 
                 var_list.append(variant)
+                ID.append(cigarette)
 
 
 
     else:
         pass
 
+
+index_list = list(range(1, len(CIGAR_list)))
 cig_df['CIGAR'] = cig_pack
-cig_df['Position'] = pos_list
-cig_df['Variant'] = var_list
+
 cig_df['Var_len'] = varlen_list
+
+
+cig_df['Position'] = pos_list
+cig_df['Position_Start'] = cig_df['Position'].astype(int) + 164419787
+cig_df['Position_Stop'] = cig_df['Position_Start'].astype(int) + cig_df['Var_len'].astype(int)
+
+cig_df['Variant'] = var_list
 cig_df['ch'] = ch_list
 
 cig_df['Variant'] = cig_df['Variant'].map({1:'I', 2:'D'})
+cig_df['ID'] = ID
+cig_df['Target'] = Target
 
-# print(cig_df)
+
+cig_df['On_Target'] = np.where(((cig_df['Target'].astype(int)).between(cig_df['Position_Start'].astype(int), cig_df['Position_Stop'].astype(int))),True,False)
+
+cig_df['Off_Target'] = np.where((cig_df['Var_len'].astype(int) > 50) & (cig_df['On_Target'] == False),True,False)
+
+cig_df = cig_df.loc[(cig_df['On_Target'] == True) | (cig_df['Off_Target'] == True) ]
+
+print(cig_df)
+
+
 insertion = cig_df.loc[(cig_df['Variant'] == 'I')]
-Ptable_ins = insertion.pivot_table(index=['Variant','Var_len'], values= ['ch'],aggfunc={'count'})
+Ptable_ins = insertion.pivot_table(index=['Variant','Var_len','ID','Position_Start','Position_Stop'], values= ['ch'],aggfunc={'count'})
 Ptable2_ins = pd.DataFrame(Ptable_ins)
 Ptable3_ins = pd.DataFrame(Ptable2_ins.to_records())
 
 
 deletion = cig_df.loc[(cig_df['Variant'] == 'D')]
-Ptable_del = deletion.pivot_table(index=['Variant','Var_len'], values= ['ch'],aggfunc={'count'})
+Ptable_del = deletion.pivot_table(index=['Variant','Var_len','ID','Position_Start','Position_Stop'], values= ['ch'],aggfunc={'count'})
 Ptable2_del = pd.DataFrame(Ptable_del)
 Ptable3_del = pd.DataFrame(Ptable2_del.to_records())
 Ptable3_del["('ch', 'count')"] = 0 - Ptable3_del["('ch', 'count')"]
@@ -101,7 +123,7 @@ df_cat.to_csv(os.path.join(path+bam+r'.csv'), sep=',', header=True,index=True)
 
 # cig_df = cig_df.iloc[0:5,]
 cig_df['Hit'] = 1/len(CIGAR_list)*100
-cig_df = cig_df[['Position','Variant','Var_len','ch','Hit']]
+cig_df = cig_df[['Position_Start','Position_Stop','Variant','Var_len','ch','Hit','ID']]
 cig_df.to_csv(os.path.join(path+bam+r'CIGAR_STRING.csv'), sep=',', header=True,index=True)
 #
 # # pink = cig_df.loc[(cig_df['Var_len'] == 1) & (cig_df['Variant'] == 'D')]
@@ -110,5 +132,6 @@ cig_df.to_csv(os.path.join(path+bam+r'CIGAR_STRING.csv'), sep=',', header=True,i
 #
 print("sample", bam)
 print("Read Count",len(CIGAR_list))
+print("Total Edits", len(np.unique(cig_df['ID'])))
 print("Script is Finished...")
 sys.exit()
